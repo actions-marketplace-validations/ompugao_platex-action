b78e1f2b84260be07d00b764c1c4da6c56902d59
Shohei Fujii <fujii.shohei@gmail.com>
2021-04-07 10:27:13 +0800

    copy latexmkrc in .devcontainer into workdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 # . = /github/workspace if actions/checkout
 set -eux
 
+if [ -f .devcontainer/.latexmkrc ]; then
+    cp .devcontainer/.latexmkrc .latexmkrc
+fi
+
 if [ ! -f .latexmkrc ]; then
     cp /.latexmkrc .latexmkrc
 fi
